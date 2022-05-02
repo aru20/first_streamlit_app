e292cae799ad1017914d64035252670199db180b
Arundhati Panigrahi <73730336+aru20@users.noreply.github.com>
2022-05-02 16:00:48 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,8 @@
 
 import streamlit
 streamlit.title('My First App')
+streamlit.title('My Parent\'s New Healthy Dinner')
+streamlit.header('Breakfast Menu')
+streamlit.text('Smoothie')
+streamlit.text('Oatmeal')
+streamlit.text('Egg')
