f5ee3bded3b5fb0c056b6e91ce8477a47b689c5d
Arundhati Panigrahi <73730336+aru20@users.noreply.github.com>
2022-05-02 16:27:20 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,13 @@
 
 import streamlit
 streamlit.title('My First App')
-streamlit.title('My Parent\'s New Healthy Dinner')
-streamlit.header('Breakfast Menu')
+streamlit.title('My Parent\'s New Healthy Dinner🍽️')
+streamlit.header('🥗 Breakfast Menu')
 streamlit.text('Smoothie')
 streamlit.text('Oatmeal')
 streamlit.text('Egg')
+streamlit.header('Breakfast Favorites')
+streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
+streamlit.text('🐔 Hard-Boiled Free-Range Egg')
+streamlit.text('🥑🍞 Avocado Toast')
