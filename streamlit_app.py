f83dffd1177430f1b340eedf34e2cd73749a5ca5
Arundhati Panigrahi <73730336+aru20@users.noreply.github.com>
2022-05-17 14:23:54 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,15 +25,25 @@ my_fruit_list = my_fruit_list.set_index('Fruit')
 fruits_selected = streamlit.multiselect("pick some fruits:" , list(my_fruit_list.index),['Avocado','Strawberries'])
 fruits_to_show = my_fruit_list.loc[fruits_selected]
 #display the table on the page
-
 streamlit.dataframe(fruits_to_show)
+#Create the repeatable code block(called Function)
+def get_fruityvice_data(this_fruit_choice):
+    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+    return  fruityvice_normalized
+
+
+
+#New section to display fruitvice api response
 streamlit.header('Fruityvice Fruit Advice!')
 try:
   fruit_choice = streamlit.text_input('What friut would you like information about?')
   if not fruit_choice:
       streamlit.error("Please select a fruit to get information.")
   else:   
-      fruityvice_response =requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+      #fruityvice_response =requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+      back_from_function = get_fruityvice_data(fruit_choice)
+      streamlit.dataframe(back_from_function)
 # Just Writes the data to the Screen
 # streamlit.text(fruityvice_response.json())
 # take the json version of the response and normalize it
@@ -47,6 +57,17 @@ my_cur = my_cnx.cursor()
 my_cur.execute("select * from pc_rivery_db.public.fruit_load_list")
 my_data_rows = my_cur.fetchall()
 streamlit.header("The fruit load list contains:")
+#snowflake_related functions
+def get_fruit_load_list():
+      with my_cnx.cursor() as my_cur:
+         my_cur.execute("select * from fruit_load_list")
+         return my_cur.fetchall()
+#add a button to load the fruit
+if streamlit.button('Get Fruit Load List'):
+      my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+      my_data_rows=get_fruit_load_lisr()
+      streamlit.dataframe(my_data_rows)
+
 streamlit.dataframe(my_data_rows)
 text_input = streamlit.text_input("What friut do you like to add","")
 streamlit.markdown(f"My fruit is:{text_input}")
