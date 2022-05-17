d6b22a88396517172ec03d8508c6c0c99b1c4751
Arundhati Panigrahi <73730336+aru20@users.noreply.github.com>
2022-05-17 16:10:32 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -47,18 +47,10 @@ try:
 except URLError as e:
    streamlit.error()
 
-
-# Just Writes the data to the Screen
-# streamlit.text(fruityvice_response.json())
-# take the json version of the response and normalize it
-      #fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-# output it on the screen as a table
-      #streamlit.dataframe(fruityvice_normalized)
-
-my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-my_cur = my_cnx.cursor()
-my_cur.execute("select * from pc_rivery_db.public.fruit_load_list")
-my_data_rows = my_cur.fetchall()
+#my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+#my_cur = my_cnx.cursor()
+#my_cur.execute("select * from pc_rivery_db.public.fruit_load_list")
+#my_data_rows = my_cur.fetchall()
 
 streamlit.header("The fruit load list contains:")
 #snowflake_related functions
