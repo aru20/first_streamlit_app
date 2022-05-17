2b58102924152c9a226592148c675aff561c8a7d
Arundhati Panigrahi <73730336+aru20@users.noreply.github.com>
2022-05-17 00:15:46 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,7 @@
 import streamlit
 import pandas
 import snowflake.connector
+
 streamlit.title('My First App')
 streamlit.title('My Parent\'s New Healthy Dinner🍽️')
 streamlit.header('🥗 Breakfast Menu')
